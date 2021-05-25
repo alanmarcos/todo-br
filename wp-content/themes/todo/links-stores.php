f816050c2818d8af1acb563e89755b5727a9ec7d
Alan Marcos <alan.marcos@credpago.com>
2021-05-25 17:03:07 -0300

    correções de css e remoção dos links vazios das stores

--- wp-content/themes/todo/links-stores.php
+++ wp-content/themes/todo/links-stores.php
@@ -3,13 +3,13 @@
 </p>
 <ul class="stores">
   <li>
-    <a href="#">
+    <!-- <a href="#"> -->
       <img src="<?php bloginfo('template_directory') ?>/images/app-store-button.svg" alt="Download na App Store" />
-    </a>
+    <!-- </a> -->
   </li>
   <li>
-    <a href="#">
+    <!-- <a href="#"> -->
       <img src="<?php bloginfo('template_directory') ?>/images/google-play-button.svg" alt="Download no Google Play" />
-    </a>
+    <!-- </a> -->
   </li>
 </ul>
