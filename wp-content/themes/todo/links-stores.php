902f6ca34967fe520ce28c7cd14106156703ba03
tekenwtf <87043118+tekenwtf@users.noreply.github.com>
2021-08-12 15:57:37 -0300

    Update links-stores.php
    atualização dos links para as lojas

--- wp-content/themes/todo/links-stores.php
+++ wp-content/themes/todo/links-stores.php
@@ -1,15 +1,15 @@
 <p class="stores-desc">
-  <strong>Disponível em breve nas lojas</strong>
+  <strong>Disponível</strong>
 </p>
 <ul class="stores">
   <li>
-    <!-- <a href="#"> -->
+    <a href="https://apps.apple.com/br/app/to-do-green/id1570834488">
       <img src="<?php bloginfo('template_directory') ?>/images/app-store-button.svg" alt="Download na App Store" />
-    <!-- </a> -->
+     </a>
   </li>
   <li>
-    <!-- <a href="#"> -->
+  <a href="https://play.google.com/store/apps/details?id=com.todo.client">
       <img src="<?php bloginfo('template_directory') ?>/images/google-play-button.svg" alt="Download no Google Play" />
-    <!-- </a> -->
+     </a> 
   </li>
 </ul>
